4e8c6bf6a9edae9df2caa178a2b48abe9fa30d8f
Beta Quark <betaquark@MacBook-Air-de-Beta-2.local>
2017-11-08 20:07:30 -0200

    first commit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Alert,
+} from 'react-native';
+import { WebBrowser, BarCodeScanner, Permissions, Constants } from 'expo';
+
+import { MonoText } from '../components/StyledText';
+import {
+  StackNavigator,
+} from 'react-navigation';
+
+export default class HomeScreen extends React.Component {
+  state = {
+    hasCameraPermission: null
+  };
+
+  componentDidMount() {
+    this._requestCameraPermission();
+  }
+
+  _requestCameraPermission = async () => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({
+      hasCameraPermission: status === 'granted',
+    });
+  };
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            $qrcode: ''
+        }
+    }
+  _handleBarCodeRead = data => {
+    this.setState({$qrcode: data.data})
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        {this.state.hasCameraPermission === null ?
+          <Text>Requesting for camera permission</Text> :
+          this.state.hasCameraPermission === false ?
+            <Text>Camera permission is not granted</Text> :
+            <BarCodeScanner
+              onBarCodeRead={this._handleBarCodeRead}
+              style={{ height: 200, width: 200 }}
+            />
+        }
+        <Text>{this.state.$qrcode}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+  }
+});
