f8cde14a139a38fe6cf3730c687c2c8af6d81274
Thiago Butignon Claramunt <thiagobutignon@MacBook-Pro-de-Thiago.local>
2017-11-09 01:53:24 -0200

    dois

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   Image,
   Platform,
@@ -7,14 +7,27 @@ import {
   Text,
   TouchableOpacity,
   View,
-  Alert,
-} from 'react-native';
-import { WebBrowser, BarCodeScanner, Permissions, Constants } from 'expo';
+  Alert
+} from "react-native";
+import { WebBrowser, BarCodeScanner, Permissions, Constants } from "expo";
 
-import { MonoText } from '../components/StyledText';
-import {
-  StackNavigator,
-} from 'react-navigation';
+import { MonoText } from "../components/StyledText";
+import { StackNavigator } from "react-navigation";
+
+global.qrcode = { data: "" };
+// Esse export deve exportar o qrcode para linkscreen.js
+//  export class qrCodeExport {
+//   static qrCode = {data:""}
+// }
+
+// import { AppColors } from './main.js';
+
+// class AnotherPage {
+//     constructor() {
+//         super();
+//         console.log(AppColors.colors); // youla! You will see your main and secondary colors :)
+//     }
+// }
 
 export default class HomeScreen extends React.Component {
   state = {
@@ -28,33 +41,28 @@ export default class HomeScreen extends React.Component {
   _requestCameraPermission = async () => {
     const { status } = await Permissions.askAsync(Permissions.CAMERA);
     this.setState({
-      hasCameraPermission: status === 'granted',
+      hasCameraPermission: status === "granted"
     });
   };
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            $qrcode: ''
-        }
-    }
   _handleBarCodeRead = data => {
-    this.setState({$qrcode: data.data})
+    this.setState({ qrcode: data.data });
   };
 
   render() {
     return (
       <View style={styles.container}>
-        {this.state.hasCameraPermission === null ?
-          <Text>Requesting for camera permission</Text> :
-          this.state.hasCameraPermission === false ?
-            <Text>Camera permission is not granted</Text> :
-            <BarCodeScanner
-              onBarCodeRead={this._handleBarCodeRead}
-              style={{ height: 200, width: 200 }}
-            />
-        }
-        <Text>{this.state.$qrcode}</Text>
+        {this.state.hasCameraPermission === null ? (
+          <Text>Requesting for camera permission</Text>
+        ) : this.state.hasCameraPermission === false ? (
+          <Text>Camera permission is not granted</Text>
+        ) : (
+          <BarCodeScanner
+            onBarCodeRead={this._handleBarCodeRead}
+            style={{ height: 200, width: 200 }}
+          />
+        )}
+        <Text>{this.state.qrcode}</Text>
       </View>
     );
   }
@@ -63,9 +71,9 @@ export default class HomeScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
+    backgroundColor: "#ecf0f1"
   }
 });
--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { ScrollView, StyleSheet, Text } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import React from "react";
+import { ScrollView, StyleSheet, Text } from "react-native";
+import { ExpoLinksView } from "@expo/samples";
 
 export default class LinksScreen extends React.Component {
   static navigationOptions = {
-    title: 'Links',
+    title: "Links"
   };
 
   render() {
@@ -12,7 +12,8 @@ export default class LinksScreen extends React.Component {
       <ScrollView style={styles.container}>
         {/* Go ahead and delete ExpoLinksView and replace it with your
            * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
+
+        <Text>{this.state.qrcode}</Text>
       </ScrollView>
     );
   }
@@ -22,6 +23,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 15,
-    backgroundColor: '#fff',
-  },
+    backgroundColor: "#fff"
+  }
 });
